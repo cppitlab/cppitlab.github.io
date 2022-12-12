ae6645689d3dcc62c1b524350b742ac89d604e16
Pruthviraj <cppitlab@protonmail.com>
2022-12-12 22:47:20 +0530

    created header and title

--- data/components/home.js
+++ data/components/home.js
@@ -5,7 +5,14 @@ export const header = new cppitlab;
 header.createElement("header");
 header.addStyle(style.headerStyle);
 header.mount("body");
+header.addAttribute("class","header");
 
+export const title = new cppitlab;
+title.createElement("h1");
+title.addStyle(style.titleStyle);
+title.addAttribute("title","C++ It Lab");
+title.addData("C++ It Lab");
+title.mount("header")
 
 
 
