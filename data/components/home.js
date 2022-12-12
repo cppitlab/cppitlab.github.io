9934b0b68ba80798f692ec40f8860fcfb47bab4e
Pruthviraj <cppitlab@protonmail.com>
2022-12-12 22:49:44 +0530

    added menu

--- data/components/home.js
+++ data/components/home.js
@@ -5,15 +5,21 @@ export const header = new cppitlab;
 header.createElement("header");
 header.addStyle(style.headerStyle);
 header.mount("body");
-header.addAttribute("class","header");
+header.addAttribute("class", "header");
 
 export const title = new cppitlab;
 title.createElement("h1");
 title.addStyle(style.titleStyle);
-title.addAttribute("title","C++ It Lab");
+title.addAttribute("title", "C++ It Lab");
 title.addData("C++ It Lab");
 title.mount("header")
 
+export const menu = new cppitlab;
+menu.createElement("div");
+menu.addAttribute("class","menu");
+menu.addStyle(style.menuStyle);
+menu.mount("header");
+menu.addData("<hr /> <hr /> <hr />")
 
 
 export { cppitlab, style };
